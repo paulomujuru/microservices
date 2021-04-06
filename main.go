6a55bc2974a208074e0bb42e6ea6c864b674fead
Paulo Junior <79360295+paulomujuru@users.noreply.github.com>
2021-04-06 17:13:56 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
-import(
+import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 )
 
-
 func main() {
-
-
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request)){
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello world")
-	}
-	http.ListenAndServe(":9000", nil)
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Ooopssss!!", http.StatusBadRequest)
+			/*
+				rw.WriteHeader(http.StatusBadRequest)
+				rw.Write([]byte("Ooooooooo"))
+			*/
+			return
+		}
+
+		fmt.Fprintf(rw, "Hello %s world \n", data)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye world")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
-
-
